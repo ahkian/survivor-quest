5011b505c9788ab89b995fbe8b514a55dceb32b7
Daniel Brazel <dbrazel4@gmail.com>
2018-06-29 16:10:11 -0400

    Got dropdown menu to work with semantic

--- src/component/quiz.js
+++ src/component/quiz.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, Input, Radio, Form } from 'semantic-ui-react'
+import { Header, Button, Input, Radio, Select, Form, Dropdown } from 'semantic-ui-react'
 
 export default class Quiz extends Component {
   constructor(){
@@ -8,7 +8,8 @@ export default class Quiz extends Component {
       lives: 10,
       question_number: 1,
       selectedQuestion: "",
-      selectedAnswers: []
+      selectedAnswers: [],
+      value: null
     }
   }
 
@@ -27,6 +28,29 @@ export default class Quiz extends Component {
     })
   }
 
+  renderAnswers = () => {
+    this.state.selectedAnswers.map(ans => {
+      return <Form.Field control={Radio} label={ans.content} key={ans.id} is-correct={ans.is_correct}/>
+    })
+  }
+
+  submitHandler = (e) => {
+    console.log(this.state.value)
+  }
+
+  changeHandler = (e) => {
+
+    this.setState({
+      value: e.target.innerText
+    }, console.log("The value is ",this.state.value, "the target is ", e.target))
+  }
+
+  formattedAnswers = () => {
+    this.state.selectedAnswers.map(ans => {
+      return {key: ans.id, is_correct: ans.is_correct.toString(), text: ans.content, id: ans.id}
+    })
+  }
+
   componentDidMount(){
     this.questionPicker()
     this.answersPicker()
@@ -37,10 +61,13 @@ export default class Quiz extends Component {
     return(
       <React.Fragment>
         <Form>
-          <Form.Group inline>
-            <label>{this.state.selectedQuestion.content}</label>
-
+          <Form.Group>
+            <Header as='h3'>{this.state.selectedQuestion.content}</Header>
+            <Dropdown onChange={this.changeHandler} placeholder="Select an answer" text={this.state.value}options={this.state.selectedAnswers.map(ans => {
+              return {key: ans.id, is_correct: ans.is_correct.toString(), text: ans.content, id: ans.id}
+            })}/>
           </Form.Group>
+          <Form.Field onClick={this.submitHandler} control={Button}>Submit</Form.Field>
         </Form>
       </React.Fragment>
     )
